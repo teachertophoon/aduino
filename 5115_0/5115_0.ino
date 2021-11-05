e9ce6355e7967fe263211f042e2052fc3d1a02b8
teachertophoon <teachertophoon@gmail.com>
2021-11-05 16:53:32 +0900

    5장 예제 코드 287쪽까지

--- 5115_0/5115_0.ino
+++ 5115_0/5115_0.ino
@@ -1,9 +1,6 @@
-// p. 275
-// p. 280
-// p. 284 내용을 참고 혹은 직접 나머지 숫자 2~9까지 7세그먼트에 출력해보세요.
+// p. 286
 const unsigned int led[7] = { 2, 3, 4, 5, 6, 7, 8 };
 
-// 2차 배열 이용
 const unsigned int num[10][7] = {
   { 1, 1, 1, 1, 1, 1, 0 }, /* 0 */
   { 0, 1, 1, 0, 0, 0, 0 }, /* 1 */
@@ -25,13 +22,11 @@ void display_init() {
 }
 
 void display_clear() {
-  // led 모두 끄기
   for (int x = 0; x < 7; x++) {
     digitalWrite(led[x], LOW);
   }
 }
 
-// p.282
 void display_number(int n) {
   if (0 <= n && n <= 9) {
     for (int x = 0; x < 7; x++) {
@@ -41,14 +36,18 @@ void display_number(int n) {
 }
 
 void setup() {
+  Serial.begin(115200);
   display_init();
 }
 
 void loop() {
-  for (int n = 0; n <= 9; n++) {
-    display_clear();
-    display_number(n);
-    delay(500);
+  if (Serial.available()) {
+    char userInput = Serial.read();
+    if ('0' <= userInput && userInput <= '9') {
+      int num = userInput - '0';
+      
+      display_clear();
+      display_number(num);
+    }
   }
 }
-// p.279
